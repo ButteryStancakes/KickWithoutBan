4d7682309d1b55ee004cd6e0d15d48718e27fea1
ButteryStancakes <jlj3322@gmail.com>
2024-05-18 17:40:25 -0400

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace KickWithoutBan
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        const string PLUGIN_GUID = "butterystancakes.lethalcompany.kickwithoutban", PLUGIN_NAME = "Kick Without Ban", PLUGIN_VERSION = "1.0.0";
+
+        void Awake()
+        {
+            new Harmony(PLUGIN_GUID).PatchAll();
+
+            Logger.LogInfo($"{PLUGIN_NAME} v{PLUGIN_VERSION} loaded");
+        }
+    }
+
+    [HarmonyPatch]
+    class KickWithoutBanPatches
+    {
+        [HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.KickPlayer))]
+        [HarmonyPostfix]
+        static void PostKickPlayer(StartOfRound __instance)
+        {
+            __instance.KickedClientIds.Clear();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Kick Without Ban
+Lethal Company plugin.
+
+Lets players rejoin after they are kicked from your lobby.
