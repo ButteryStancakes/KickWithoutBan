9a9389276a495a638ba80d2e0eb63925768b316d
ButteryStancakes <jlj3322@gmail.com>
2025-04-21 03:07:51 -0400

    Lobby Compatibility

--- Plugin.cs
+++ Plugin.cs
@@ -4,9 +4,12 @@ using HarmonyLib;
 namespace KickWithoutBan
 {
     [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    [BepInDependency(GUID_LOBBY_COMPATIBILITY, BepInDependency.DependencyFlags.SoftDependency)]
     public class Plugin : BaseUnityPlugin
     {
-        const string PLUGIN_GUID = "butterystancakes.lethalcompany.kickwithoutban", PLUGIN_NAME = "Kick Without Ban", PLUGIN_VERSION = "1.0.0";
+        internal const string PLUGIN_GUID = "butterystancakes.lethalcompany.kickwithoutban", PLUGIN_NAME = "Kick Without Ban", PLUGIN_VERSION = "1.0.1";
+
+        const string GUID_LOBBY_COMPATIBILITY = "BMX.LobbyCompatibility";
 
         void Awake()
         {
